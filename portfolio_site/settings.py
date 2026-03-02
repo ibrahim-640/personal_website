ec6965d6941f8911ce4fdc1d0bddc9c41807d947
ibrahim-640 <mwitaibrahim88@gmail.com>
2026-03-02 09:45:41 +0300

    Fix settings

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
+# Get ALLOWED_HOSTS from env; default works locally
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
