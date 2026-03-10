26da414b36b19532e09deb6d10b7910ca07ecf76
Ibrahim Portfolio <mwitaibrahim88@gmail.com>
2026-03-10 20:11:29 +0300

    fixed image url

--- portfolio_site/portfolio_site/settings.py
+++ portfolio_site/portfolio_site/settings.py
@@ -41,7 +41,7 @@ ALLOWED_HOSTS = os.environ.get(
     "localhost,127.0.0.1"
 ).split(",")
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 USE_X_FORWARDED_HOST = True
 
 # Application definition
@@ -166,13 +166,13 @@ if not EMAIL_HOST_USER or not EMAIL_HOST_PASSWORD:
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-if not DEBUG:
+# Enable HTTPS security only in production
+if not DEBUG and "RENDER" in os.environ:
     SECURE_SSL_REDIRECT = True
 
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
 
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
+    # SECURE_HSTS_SECONDS = 31536000
+    # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    # SECURE_HSTS_PRELOAD = True
