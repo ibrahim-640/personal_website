03f8dd0515b741314ecc61da0ad974b81754fc9c
ibrahim-640 <mwitaibrahim88@gmail.com>
2026-03-17 21:16:46 +0300

    Add build script

--- build.sh
+++ build.sh
@@ -1,13 +1,6 @@
-#!/usr/bin/env bash
-
-# Upgrade pip (optional but recommended)
-python3 -m pip install --upgrade pip
-
-# Install dependencies
+#!/bin/bash
+pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
-
-# Collect static files
-python3 manage.py collectstatic --noinput
-
-# Apply migrations to create all tables, including main_project
-python3 manage.py migrate --noinput
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
